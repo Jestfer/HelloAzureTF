a071dfd549a444428372e7c0e210c6b7674e53d7
Jestfer <jestfer6@gmail.com>
2021-02-04 20:13:54 +0000

    fixed formatting for acr-create file

--- acr-create.tf
+++ acr-create.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                     = "containerRegistry1"
-  resource_group_name      = "group1-tf"
-  location                 = "westeurope"
+  name                = "containerRegistry1"
+  resource_group_name = "group1-tf"
+  location            = "westeurope"
 }
