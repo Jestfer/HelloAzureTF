4df80907db8067654f10ea2019e51b692d987010
Jestfer <jestfer6@gmail.com>
2021-02-04 20:47:46 +0000

    added standard sku

--- acr-create.tf
+++ acr-create.tf
@@ -2,4 +2,5 @@ resource "azurerm_container_registry" "acr" {
   name                = "group1containerRegistry"
   resource_group_name = "group1-tf"
   location            = "westeurope"
+  sku                 = "Standard"
 }
